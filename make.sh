da04fce18e65f93b59ba68a8c78a3f98d067b9a1
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-04-05 16:08:32 +0800

    Update

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euvxo pipefail
 [ -s topics.txt ] || pg
-shuf < topics.txt > a
-first < a > b
-grep -v "$(<b)" a > topics.txt || true
-mv b topic.txt
-rm a
-exg2 < topic.txt > exg.txt
-TTS=tts-1-hd tts exg.mp3 < exg.txt
+shufi topics.txt
+draw topics.txt > topic.txt
+TEMP= MODEL=o3-mini HIGH=1 exg2 < topic.txt > exg.txt
+voice audio.mp3 < exg.txt
+sens < exg.txt > sens.txt
+addnl < sens.txt > text.txt
+cp topic.txt title.txt
