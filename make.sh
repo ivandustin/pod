75907b11e78adde45ecc52f4a6c47134caf00ef6
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-04-05 20:59:52 +0800

    Use cpex

--- make.sh
+++ make.sh
@@ -3,8 +3,10 @@ set -euvxo pipefail
 [ -s topics.txt ] || pg
 shufi topics.txt
 draw topics.txt > topic.txt
-TEMP= MODEL=o3-mini HIGH=1 exg2 < topic.txt > exg.txt
-voice audio.mp3 < exg.txt
-sens < exg.txt > sens.txt
+cpg
+TEMP= MODEL=o3-mini HIGH=1 exg2 < edit.txt > exg.txt
+cat edit.txt exg.txt > cat.txt
+sens < cat.txt > sens.txt
 addnl < sens.txt > text.txt
+tts audio.mp3 < text.txt
 cp topic.txt title.txt
